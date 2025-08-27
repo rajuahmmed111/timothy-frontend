89e94c6b1047a2bd3984fb34195753cc47204c53
TAIJULAMAN <taijulaman8@gmail.com>
2025-08-27 22:50:58 +0600

    MostVisitedHotels  completed

---
+++ src/pages/Home/ExploreAllServices/ExploreAllServices.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+export default function ExploreAllServices() {
+    return (
+        <section className="py-16 container mx-auto">
+            <div className="container mx-auto px-4">
+                <div className="text-center mb-12">
+                    <h2 className="text-sm font-medium text-gray-600 uppercase text-start">Explore All Services</h2>
+                </div>
+            </div>
+        </section>
+    )
+}
