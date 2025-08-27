8c078dd10d5a4f15af6c9331d88cb007948d3e64
TAIJULAMAN <taijulaman8@gmail.com>
2025-08-27 21:30:53 +0600

    hero and impact completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import Hero from './Hero/Hero';
+import Impact from './Impact/Impact';
 export default function Home() {
-    return <div>Home</div>;
+    return <div>
+        <Hero />
+        <Impact />
+    </div>;
 }
