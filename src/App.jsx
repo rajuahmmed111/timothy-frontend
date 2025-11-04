decbfb14324ef73a8ad28fac504c82f77693f3e6
ahteshamulhasan <ahteshamulhasan2@gmail.com>
2025-11-04 23:30:01 +0600

    Hotel Booking create successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className="text-3xl text-teal-500 font-bold text-center mt-[100px]">
-      react-starter-template
-    </div>
+    <>
+      <div className="text-3xl text-teal-500 font-bold text-center mt-[100px]">
+        react-starter-template
+      </div>
+      <Toaster position="top-center" />
+    </>
   );
 }
 
---
+++ toast.jsx
@@ -0,0 +1,23 @@
+import { ToastContainer, toast } from "react-toastify";
+export const handleSuccess = (message) => {
+  toast.success(message, {
+    position: "bottom-left",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+export const handleError = (message) => {
+  toast.error(message, {
+    position: "bottom-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
