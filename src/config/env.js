c0037e1fe4a4650a886b5b7f31ec9d3053bfcac2
Raju Ahmmed <ahraju2003@gmail.com>
2025-11-23 23:07:32 +0600

    add AWS base url

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,4 @@
 
 // export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://timothy-backend.onrender.com/api/v1' ;
-export const API_BASE_URL = "https://timothy-backend.onrender.com/api/v1";
-// export const API_BASE_URL = "https://api.fasifys.com/api/v1";
-
-
+// export const API_BASE_URL = "https://timothy-backend.onrender.com/api/v1";
+export const API_BASE_URL = "https://api.fasifys.com/api/v1";
