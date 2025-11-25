337b2db7f15ea5d520ef99e8d40e0462686d0a66
ahteshamulhasan <ahteshamulhasan2@gmail.com>
2025-11-25 09:26:51 +0600

    hotel part all most done

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,3 @@
-
-export const API_BASE_URL =  "https://certification-ace-accept-frank.trycloudflare.com/api/v1"
-// export const API_BASE_URL = "https://timothy-backend.onrender.com/api/v1";
+// export const API_BASE_URL =  "https://certification-ace-accept-frank.trycloudflare.com/api/v1"
+export const API_BASE_URL = "https://timothy-backend.onrender.com/api/v1";
 // export const API_BASE_URL = "https://api.fasifys.com/api/v1";
-
-
